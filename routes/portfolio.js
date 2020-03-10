383b6f5371695459ae2e31a83f46eec500306922
Jared <jared@digitalnutt.com>
2020-03-10 09:52:56 -0700

    getting different client routing to work

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -2,21 +2,20 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
-const staticMiddleware = express.static(path.join(__dirname, '..', 'portfolio','loanamort','build'));
-
+// const staticMiddleware = express.static(path.join(__dirname, '..', 'portfolio','loanamort','build'));
 
 router.use('/loanamort', (req, res, next) => {
-    next();
-})
+  next();
+});
 
 router.use('/loanamort/', (req, res, next) => {
-    staticMiddleware(req, res, next);
-})
+  staticMiddleware(req, res, next);
+});
 
 router.get('/loanamort/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'portfolio','loanamort','build', 'index.html'));
-    console.log(path.join(__dirname, '..', 'portfolio','loanamort','build'))
-    // res.sendFile('/index.html');
+  // res.sendFile(path.join(__dirname, '..', 'portfolio','loanamort','build', 'index.html'));
+  // console.log(path.join(__dirname, '..', 'portfolio','loanamort','build'))
+  // res.sendFile('/index.html');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,41 +2,59 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-const blogposts = require('./routes/api/blogposts');
+// const blogposts = require('./routes/api/blogposts');
 const portfolio = require('./routes/portfolio');
 
-const keys = require('./config/keys');
+// const keys = require('./config/keys');
 
 const app = express();
 
 //----- DB CONFIG -----//
-const db = keys.mongoURI;
+// const db = keys.mongoURI;
 
 //----- CONNECT TO DB -----//
-mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('DB connection successful!'))
-    .catch(err => console.log(err));
+// mongoose
+//   .connect(db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('DB connection successful!'))
+//   .catch(err => console.log(err));
 
 //----- BODY PARSE MIDDLEWARE -----//
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
 //----- USE ROUTES -----//
-app.use('/api/blogposts', blogposts);
+app.get('/', (req, res, next) => {
+  res.sendFile('index.html');
+});
+
+app.get('/portfolio', (req, res, next) => {
+  app.use(
+    express.static(path.join(__dirname, 'portfolio', 'loanamort', 'build')),
+  );
+  const options = {
+    root: path.join(__dirname, 'portfolio', 'loanamort', 'build'),
+  };
+
+  // app.use(express.static());
+  res.sendFile('index.html', options);
+});
+// app.use('/api/blogposts', blogposts);
 
 // const staticMiddleware = express.static(path.join(__dirname,'portfolio','loanamort','build'));
 
+// app.use(
+//   '/portfolio',
+//   // express.static(path.join(__dirname, 'portfolio', 'loanamort', 'build')),
+// );
 
-
-app.use('/route1/', express.static(path.join(__dirname,'portfolio','loanamort','build')));
-
-
-app.use('/portfolio', portfolio);
+// app.use('/portfolio', portfolio);
 
 //----- START SERVER -----//
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
